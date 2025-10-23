fcef21fad69dee018320e9d8ed4d6b18215a7245
GuilhermeVarellaa <guilhermeraul1@hotmail.com>
2025-10-23 16:54:52 -0300

    fix(router): importar RouterOutlet no App e RouterLink no Login para navegação funcionar

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class LoginComponent {
+
+}
